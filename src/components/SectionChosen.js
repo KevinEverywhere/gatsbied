53ef8631d3f6967caa697e3eeb7544d3aaa1822c
Kevin Ready <kevin.ready@gmail.com>
2019-02-20 12:55:23 -0800

    stable, fixed smlogo

--- src/components/SectionChosen.js
+++ src/components/SectionChosen.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ItemChooser from './ItemChooser'
+import { Link } from 'gatsby'
 
 const SectionChosen = props => (
   <nav>
+    <Link to="/">
+      <div className="smlogo">
+          <span className="icon fa-bicycle"></span>
+      </div>
+    </Link>
     <ul>
       <ItemChooser item="Work" />
       <ItemChooser item="Creative" />
