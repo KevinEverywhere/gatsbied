4eebf02eaaa6eacd97480fc808ab054500e2abc9
Kevin Ready <kevin.ready@gmail.com>
2019-03-25 16:43:47 -0700

    Updates

--- sw.js
+++ sw.js
@@ -29,14 +29,14 @@ self.__precacheManifest = [
     "url": "webpack-runtime-cf4477fa2f1d788b140c.js"
   },
   {
-    "url": "app-0b8c5d835d94d3784da5.js"
+    "url": "app-c964bc610a99ce893145.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-538c2471dc8d9ca14b93.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2d80d2ab7fdf00c1213e93117b6266fb"
+    "revision": "5e8b4fd5aabb9fddf01d34ad6cef0493"
   },
   {
     "url": "2.13e139671fe6929491ee.css"
