0530aedaa3f2203915058ec23abe48a73df96e2a
Kevin Ready <kevin.ready@gmail.com>
2019-03-27 17:17:44 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-87f2bb9da48b656b58ff.js"
+    "url": "webpack-runtime-3b5f68a4b6162879ead5.js"
   },
   {
     "url": "app-a5b7181adabc735478b4.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "704a1cf7851b8ea7cb21e616ef10dbe4"
+    "revision": "79852d3317c93c9fe9b5a7ce5949c484"
   },
   {
     "url": "2.13e139671fe6929491ee.css"
